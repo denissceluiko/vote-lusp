6cc9b83eed2cc52f4676643f23cf8686295cdfc6
Deniss Celuiko <deniss.celuiko@gmail.com>
2020-11-02 12:45:49 +0200

    Fixed queue

--- resources/views/voter/create.blade.php
+++ resources/views/voter/create.blade.php
@@ -7,7 +7,7 @@
                 <h1>2020. gada Latvijas Universitātes fakultāšu studentu pašpārvalžu vēlēšanas</h1>
                 <p>Ņemot vērā to, ka šobrīd studijas notiek pārsvarā attālināti, pašpārvalžu vēlēšanas arī nolemts pārcelt uz tiešsaistes vidi.</p>
                 <p>Lai saņemtu savu vēlēšanu zīmi, ievadi zemāk savu studenta apliecības numuru, nospied "Saņemt" un pārbaudi savu studenta e-pastu (<a href="https://www.lu.lv/epasts" target="_blank">lu.lv/epasts</a>). E-pastā būs tālākas instrukcijas balsojuma veikšanai.</p>
-                <p class="my-2 text-danger">Šobrīd ir zināma problēma saņemt e-pastus tiem, kam stud. apl. nr. atšķiras no LUIS logina, šī problēma tiek risināta.</p>
+                <h4 class="my-2 text-danger">Šobrīd ir zināma problēma saņemt e-pastus tiem, kam stud. apl. nr. atšķiras no LUIS logina, šī problēma tiek risināta.</h4>
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">Saņem savu vēlēšanu zīmi</h5>
