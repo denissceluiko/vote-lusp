5827b57107970b7c5a8caf848915f4ec850d9ce4
Deniss Celuiko <deniss.celuiko@gmail.com>
2020-12-11 21:09:53 +0200

    Add user table seeder

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin',
+            'role' => 'admin',
+            'password' => bcrypt('changeme'),
+        ]);
+    }
+}
